7b1ffdb9d5a20baf07ba8b710d829b538c15f01c
mailsonsoc <albuquerque.soc@gmail.com>
2024-11-26 18:38:09 -0300

    project structure

---
+++ src/main/java/com/impact/monitor/IaImpactMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.impact.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaImpactMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaImpactMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/impact/monitor/IaImpactMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impact.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaImpactMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
